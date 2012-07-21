0c03c3cfce4c6032e7485a409106568c6d87cc92
Linda Tong <linda.tong@gmail.com>
2012-07-21 10:48:10 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myselector::Application.config.secret_token = '241e01682af3f888c779c0ec65b4c550a0c6fe94de6cfda5c1c650e00e53163762a028ef0b857d51b743ae4e0a66c9c0c7472fae0fb182325a51c24ed6ecad53'
