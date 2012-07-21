426862f163398f65be6808fbf3647376081d8816
Linda Tong <linda.tong@gmail.com>
2012-07-21 14:00:37 -0700

    added voting machinery and created vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,8 @@
 class VotesController < ApplicationController
+def create
+  topic = Topic.find(params[:topic_id]) #find the active topic
+  vote = topic.votes.build #create a vote
+  vote.save! #save the vote
+  redirect_to(topics_path) #send back to index page
+end
 end
