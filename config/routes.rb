da8a1c628e94d772616133d9aafebe20141f3ef1
Linda Tong <linda.tong@gmail.com>
2012-07-21 14:35:25 -0700

    made it prettier

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Myselector::Application.routes.draw do
   resources :votes
 
   resources :topics
-
+  root :to => 'topics#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
